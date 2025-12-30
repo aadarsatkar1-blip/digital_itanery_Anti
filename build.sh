5fb393972728dfde946ac071f80724216db7409a
[company system] <[aadarsatkar1@gmail.com]>
2025-12-30 12:06:19 +0530

    Initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+npm install
+npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
+python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py create_superuser_if_none
+
+
